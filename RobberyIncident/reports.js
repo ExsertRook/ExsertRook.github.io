c84a5e265b87ab95071f0a6a94eefc8b38ebd307
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-06-27 23:42:44 +0300

    Update reports.js

--- RobberyIncident/reports.js
+++ RobberyIncident/reports.js
@@ -99,7 +99,7 @@ function report() {
 	let plate = document.getElementById('vehicleplate').value;
 	let vehicledesc = document.getElementById('vehicledesc').value;
 	let vehicleregistration = document.getElementById('vehiclereg').value;
-	if (vehicledesc) vehicledesc = ` which was a ${vehicledesc}${(plate ? ' (PLATE: ' + plate + ')' : '')}${(vehicleregistration ? ' Vehicle was registered to an individual named: ' + vehicleregistration + '.' : ''`)};
+	if (vehicledesc) vehicledesc = ` which was a ${vehicledesc}${(plate ? ' (PLATE: ' + plate + ')')}${(vehicleregistration ? ' Vehicle was registered to an individual named: ' + vehicleregistration + '.' : ''`)};
 	buffer.push(`We then let them get into their vehicle${vehicledesc}. The vehicle was registered to an individual namedOnce everyone was ready, the chase started and they attempted to evade from police recklessly.`);
 	buffer.push(`The chase lasted for a bit of time until `);
 	buffer.push('');
