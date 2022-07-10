2ded9e95a4095b593a18dbb06acdf41eefaedc25
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-07-10 06:23:40 +0300

    Update reports.js

--- drugarrest/reports.js
+++ drugarrest/reports.js
@@ -38,7 +38,7 @@ function report() {
 	buffer.push(pospicInformation[pospicresult].text);
 	buffer.push('');
 
-	buffer.push('The suspect did not make an attempt to run and we managed to talk to them. Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back positive for drug residue.');	
+	buffer.push('The suspect did not make an attempt to run and we managed to talk to them. Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back positive for drug residue. The suspect was then placed into handcuffs.');
 
 	let drugSelected = document.getElementById('drugtype');
 	let drugInformation = {
