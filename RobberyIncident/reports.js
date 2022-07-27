ead6f0dac9d1dd9d282dc309d929f7d6bbfb7851
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-07-27 07:16:14 +0300

    Update reports.js

--- RobberyIncident/reports.js
+++ RobberyIncident/reports.js
@@ -196,10 +196,10 @@ function report() {
 			text: 'The chase lasted for a bit of time until',
 		},
 		'Vehicle was sent into water | Caught': {
-			text: 'The chase lasted for a bit of time until',
+			text: 'The chase lasted for a bit of time until they had drove their vehicle into the water. Eventually after pursuing the suspects in the water, they were all apprehended and placed under police custody.',
 		},
 		'Vehicle was sent into water | Escaped': {
-			text: 'The chase lasted for a bit of time until',
+			text: 'The chase lasted for a bit of time until they had drove their vehicle into the water. Eventually after pursuing the suspects in the water, we lost eyes on all the fleeing suspects and they have escaped under police custody.',
 		},
 		'Vehicle blew up': {
 			text: 'The chase lasted for a bit of time until they had drove so recklessly to the point where they had accidentally hit an explosive and blew up their vehicle. After recieving medical attention, the suspects were apprehended and placed under police custody.',
