ed133b04f2691b88bb9eec49493d816b0824586a
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-07-30 05:09:09 +0300

    Update reports.js

--- trafficstop/reports.js
+++ trafficstop/reports.js
@@ -20,7 +20,7 @@ function report() {
 	buffer.push(`[VEHICLE | CHASE]:`);
 	let plate = document.getElementById('vehicleplate').value;
 	let vehicledesc = document.getElementById('vehicledesc').value;
-	let vehiclereg = document.getElementById('vehiclereg').value
+	let vehiclereg = document.getElementById('vehiclereg').value;
 	if (vehicledesc) vehicledesc = ` which was a ${vehicledesc}${(plate ? ' (PLATE: ' + plate + ')' : '')}`;
 	buffer.push(`We then let them get into their vehicle${vehicledesc}. The vehicle was registered to an individual named ${vehiclereg}. Once everyone was ready, the chase started and they attempted to evade from police recklessly.`);
 	buffer.push(`The chase lasted for a bit of time until (write later how it ended)`);
