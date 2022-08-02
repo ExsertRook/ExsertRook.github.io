addee21564919906285b42d5fd78e9af52e12418
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-08-02 18:20:56 +0300

    Update reports.js

--- trafficstop/reports.js
+++ trafficstop/reports.js
@@ -20,7 +20,7 @@ function report() {
 	
 	buffer.push(`[STOP INFORMATION]:`);
 	let location = document.getElementById('location').value;
-	if (location) buffer.push(`During routine patrol, we pulled over a vehicle next to ${location}.`);\
+	if (location) buffer.push(`During routine patrol, we pulled over a vehicle next to ${location}.`);
 	
 	let stopreasonSelected = document.getElementById('stopreason');
 	let stopreasonInformation = {
