1f58a5427c617f43da55c7f1f6127839f1e06a6c
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-08-28 05:13:16 +0300

    Create reports.js

---
+++ trespassing/reports.js
@@ -0,0 +1,282 @@
+'use strict';
+
+let buffer = [];
+let officersInvolved = new Set();
+let darkmodeState;
+let alreadySpecifiedRobbery = false;
+let ROBBERY_STATE = 'JEWLERY';
+
+function report() {
+	let callsign = document.getElementById('yourself').value.trim();
+	if (callsign) localStorage.setItem('callsign', callsign);
+	if (!callsign) callsign = '[missing]';
+	const ind = "        ";
+	let date = new Date().toLocaleDateString('en-US');
+
+	buffer = [];
+	buffer.push("[REPORTING OFFICER]:");
+	buffer.push(callsign);	
+
+	let location = document.getElementById('location').value;
+	let processedat = document.getElementById('processedat').value;
+	buffer.push('');
+	
+	buffer.push(`[DETAILS OF THE INCIDENT]:`);
+	if (location) buffer.push(`While being out on regular patrol, we responded to 10-66 dispatch calls at ${location}. When we arrived at the general vicinity, we started looking for anybody that was doing any sort of suspicious activities.`);
+	
+	let pospicSelected = document.getElementById('pospic');
+	let pospicInformation = {
+		'Yes': {
+			text: 'After some time had passed, we managed to witness an individual walking up to the pedestrians passing by and handing something over to them. Once we managed to take a picture of the positive handoff, we moved in to approach the suspect in question.',
+		},
+		'No': {
+			text: 'After some time had passed, we managed to witness an individual walking up to the pedestrians passing by and handing something over to them. Fearing that the suspect may leave the area, we moved in quickly without taking a picture of the positive handoff.',
+		}
+	};
+	
+	let pospicresult = pospicSelected.options[pospicSelected.selectedIndex].text;
+	buffer.push(pospicInformation[pospicresult].text);
+	buffer.push('');
+
+	let susranSelected = document.getElementById('susran');
+	let susranInformation = {
+		'Yes': {
+			text: 'Once we moved in to talk to the suspect, they made an attempt to run but they were eventually apprehended and placed under police detainment.',
+		},
+		'No': {
+			text: 'Once we moved in to talk to the suspect, they did not make an attempt to run and we had them detained for the time being until we gained more information about the scene.',
+		}
+	};
+	
+	let susranresult = susranSelected.options[susranSelected.selectedIndex].text;
+	buffer.push(susranInformation[susranresult].text);
+	
+	let testresultsSelected = document.getElementById('testresults');
+	let testresultsInformation = {
+		'Negative': {
+			text: 'Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back negative for any drug residue.',
+		},
+		'Minor amounts': {
+			text: 'Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back positive for minor amounts of drug residue.',
+		},
+		'Moderate amounts': {
+			text: 'Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back positive for moderate amounts of drug residue.',
+		}
+	};
+	
+	let testresultsresult = testresultsSelected.options[testresultsSelected.selectedIndex].text;
+	buffer.push(testresultsInformation[testresultsresult].text);
+
+	let drugSelected = document.getElementById('drugtype');
+	let drugInformation = {
+		'Weed': {
+			text: 'The drug specifically in question was weed.',
+		},
+		'Cocaine': {
+			text: 'The drug specifically in question was cocaine.',
+		},
+		'Oxy': {
+			text: 'The drug specifically in question was oxy.',
+		},
+		'Acid': {
+			text: 'The drug specifically in question was acid.',
+		},
+		'Meth': {
+			text: 'The drug specifically in question was meth.',
+		}
+	};
+	
+	let drugresult = drugSelected.options[drugSelected.selectedIndex].text;
+	buffer.push(drugInformation[drugresult].text);
+	buffer.push('');
+	
+	let medicalSelected = document.getElementById('medicalattention');
+	let medicalInformation = {
+		'Was requested by multiple suspects': {
+			label: 'WAS REQUESTED',
+			text: 'After we apprehended the suspects, they requested medical attention. We then transported them to Saint Fiacre where they got further medical attention.',
+		},
+		'Was requested by one suspect': {
+			label: 'ONE REQUESTED',
+			text: 'After we apprehended the suspects, one of them requested or needed medical attention. We then transported that suspect to Saint Fiacre where they got further medical attention.',
+		},
+		'Was not requested or needed': {
+			label: 'WAS NOT REQUESTED',
+			text: 'After we apprehended the suspects, they did not request or need any medical attention.',
+		}
+	};
+	let medical = medicalSelected.options[medicalSelected.selectedIndex].text;
+	buffer.push(`[MEDICAL ATTENTION | ${medicalInformation[medical].label}]:`);
+	buffer.push(medicalInformation[medical].text);
+	buffer.push('');
+
+	let processed = document.getElementById('processedat').value;
+	buffer.push('[PROCESSED]:');
+	buffer.push(`All of the apprehended suspects were processed at ${processed}.`);
+	
+	let curDarkmode = document.getElementById('darkmode').checked;
+	if (curDarkmode) {
+		if (darkmodeState === 'false') updateDarkmode();
+	} else if (!curDarkmode) {
+		if (darkmodeState === 'true') updateDarkmode();
+	}
+
+	return document.getElementById('reportBody').innerHTML = buffer.join("\n");
+}
+
+let inputs = document.querySelectorAll('input[type="text"], input[type="number"], textarea');
+inputs.forEach(i => i.addEventListener('keyup', report, false));
+
+let checkboxes = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
+checkboxes.forEach(i => i.addEventListener('click', report, false));
+
+let selectOptions = document.querySelectorAll('select');
+selectOptions.forEach(i => i.addEventListener('click', report, false));
+
+function loadName() {
+	let callsign = '';
+	if (localStorage.getItem('callsign')) callsign = localStorage.getItem('callsign');
+	document.getElementById('yourself').value = callsign;
+}
+
+// Listen for a click on the button
+function updateDarkmode() {
+	// Then toggle (add/remove) the .dark-theme class to the body
+	let darkmode = document.getElementById('darkmode').checked;
+	if (darkmode) {
+		localStorage.setItem("darkmode", true);
+		darkmodeState = 'true';
+	} else if (!darkmode) {
+		localStorage.setItem("darkmode", false);
+		darkmodeState = 'false';
+	}
+	document.body.classList.toggle('dark-theme');
+}
+
+function loadDarkmode() {
+	let darkmodeSetting = localStorage.getItem("darkmode");
+	if (!darkmodeSetting || darkmodeSetting === 'undefined' || darkmodeSetting === 'false') {
+		localStorage.setItem("darkmode", false);
+		darkmodeState = 'false';
+	}
+	if (darkmodeSetting == 'true') {
+		document.getElementById('darkmode').checked = true;
+		document.body.classList.toggle('dark-theme');
+		darkmodeState = 'true';
+	}
+	loadName();
+	if (ROBBERY_STATE === 'JEWLERY') {
+		document.getElementById('whatFleeca').style.display = 'none';
+		document.getElementById('whatStore').style.display = 'none';
+	}
+	//loadOfficers();
+}
+
+let officers = null;
+let matched = [];
+
+const replaceNames = {
+	'Bucky Killbourne': 'Bucky Langston',
+	'Xander Langston': 'Xander Killbourne'
+};
+
+function loadOfficers() {
+	let cachedOfficers = localStorage.getItem("officers");
+	if (!officers) {
+		let xhr = new XMLHttpRequest();
+		try {
+			xhr.open("GET", "https://celestial.network/legacyrp/sasp", false);
+			xhr.send(null);
+
+			officers = JSON.parse(xhr.responseText).data;
+			officers = officers.map(officer => officer.callsign + ' ' +
+				(replaceNames[officer.full_name] ? replaceNames[officer.full_name] : officer.full_name));
+			localStorage.setItem('officers', xhr.responseText);
+		} catch (e) {
+			if (cachedOfficers) {
+				cachedOfficers = JSON.parse(cachedOfficers).data;
+				officers = cachedOfficers.map(officer => officer.callsign + ' ' + officer.full_name);
+				alert('Failed to load officers data from roster; using cached officers data...');
+			} else {
+				alert('Failed to load officers data from roster & no cache value stored!');
+			}
+		}
+	}
+}
+
+function searchOfficer(search) {
+	if (!search) {
+		document.getElementById('officerslist').innerHTML = '';
+		return;
+	}
+	search = search.toLowerCase();
+
+	if (!officers) loadOfficers();
+
+	let results = officers.filter(officer => officer.toLowerCase().includes(search));
+	let resultsCap = 5;
+	let count = 0;
+	let finalResults = [];
+	results.forEach(result => {
+		count++;
+		if (count > resultsCap) return;
+		result = result.trim();
+		finalResults.push("<button title='Add this officer to the list of officers involved' onClick='toggleOfficer(\"" + result + "\")'>" + result + "</button>");
+	});
+	document.getElementById('officerslist').innerHTML = finalResults.join("<br />");
+}
+
+function toggleOfficer(id) {
+	if (officersInvolved.has(id)) {
+		console.log("Removing " + id + "...");
+		officersInvolved.delete(id);
+	} else {
+		console.log("Adding " + id + "...");
+		officersInvolved.add(id);
+
+		document.getElementById('officersearch').value = "";
+	}
+	report();
+	updateOfficers();
+}
+
+function updateOfficers() {
+	let output = "";
+	for (let id of officersInvolved.values()) {
+		output += `<div class="chip">\n`;
+		output += `<img src="images/hat2.png" width="96" height="96">\n`;
+		output += `${id}\n`;
+		output += `<span class="closebtn" title="Remove this officer from the list of officers involved" style="cursor: default;" onclick='toggleOfficer(\"${id}\")'><i class="fa fa-times-circle-o" aria-hidden="true"></i>
+</span>\n`;
+		output += `</div>`
+	}
+
+	document.getElementById('officersAdded').innerHTML = "<br />" + output;
+}
+
+function showCopiedPopup() {
+	let popup = document.getElementById("myPopup");
+	popup.classList.toggle("show");
+	setTimeout(function() {
+		popup.classList.toggle("show");
+	}, 3500);
+}
+
+document.getElementById('copyReport').addEventListener('click', copy, false);
+function clearSelection() {
+	if (window.getSelection) {
+		window.getSelection().removeAllRanges();
+	} else if (document.selection) {
+		document.selection.empty();
+	}
+}
+function copy() {
+	document.getElementById('reportBody').select();
+	try {
+		document.execCommand('copy');
+		showCopiedPopup();
+		clearSelection();
+	} catch(e) {
+		console.log("Copy error: " + e);
+	}
+}
