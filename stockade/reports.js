4e8ad33d05ece1eebf58e016e43e23ce5815ce14
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-08-29 22:11:59 +0300

    Update reports.js

--- stockade/reports.js
+++ stockade/reports.js
@@ -21,6 +21,7 @@ function report() {
 	buffer.push(`[STOP INFORMATION]:`);
 	let location = document.getElementById('location').value;
 	if (location) buffer.push(`During routine patrol, we pulled over a vehicle next to ${location}.`);
+	buffer.push('');
 	
 	buffer.push(`[VEHICLE DESCRIPTION]:`);
 	let plate = document.getElementById('vehicleplate').value;
