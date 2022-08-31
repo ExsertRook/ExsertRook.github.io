d24a12e82aa6c662e9b40dcd8bb11aa7a90af445
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-08-31 12:44:03 +0300

    Update reports.js

--- trespassing/reports.js
+++ trespassing/reports.js
@@ -22,72 +22,56 @@ function report() {
 	buffer.push('');
 	
 	buffer.push(`[DETAILS OF THE INCIDENT]:`);
-	if (location) buffer.push(`While being out on regular patrol, we responded to 10-66 dispatch calls at ${location}. When we arrived at the general vicinity, we started looking for anybody that was doing any sort of suspicious activities.`);
+	buffer.push(`While taking a small break from patrolling, we had noticed an individual who was not an officer on the premises of MRPD where they should not be.`);
 	
-	let pospicSelected = document.getElementById('pospic');
-	let pospicInformation = {
-		'Yes': {
-			text: 'After some time had passed, we managed to witness an individual walking up to the pedestrians passing by and handing something over to them. Once we managed to take a picture of the positive handoff, we moved in to approach the suspect in question.',
+	let locationSelected = document.getElementById('location');
+	let locationInformation = {
+		'Inside of MRPD': {
+			text: 'We initially spotted the trespasser inside of MRPD.',
 		},
-		'No': {
-			text: 'After some time had passed, we managed to witness an individual walking up to the pedestrians passing by and handing something over to them. Fearing that the suspect may leave the area, we moved in quickly without taking a picture of the positive handoff.',
+		'Roof': {
+			text: 'We initially spotted the trespasser on the roof of MRPD.',
+		},
+		'Side Parking Lot': {
+			text: 'We initially spotted the trespasser outside at the side parking lot.',
+		},
+		'Carpool': {
+			text: 'We initially spotted the trespasser downstairs inside the carpool.',
+		},
+		'Back Parking Lot': {
+			text: 'We initially spotted the trespasser outside at the parking lot behind MRPD where we bring in suspects.',
 		}
 	};
 	
-	let pospicresult = pospicSelected.options[pospicSelected.selectedIndex].text;
-	buffer.push(pospicInformation[pospicresult].text);
+	let locationresult = locationSelected.options[locationSelected.selectedIndex].text;
+	buffer.push(locationInformation[locationresult].text);
+	
+	let warnedSelected = document.getElementById('warned');
+	let warnedInformation = {
+		'Yes': {
+			text: 'The individual who was trespassing was warned to leave the area otherwise he would get arrested.',
+		},
+		'No': {
+			text: 'Since the indivudal was posing a threat, we skipped warning him and went in for the arrest as the suspect themselves knew that they were trespassing.',
+		}
+	};
+	
+	let warnedresult = warnedSelected.options[warnedSelected.selectedIndex].text;
+	buffer.push(warnedInformation[warnedresult].text);
 	buffer.push('');
-
+	
 	let susranSelected = document.getElementById('susran');
 	let susranInformation = {
 		'Yes': {
-			text: 'Once we moved in to talk to the suspect, they made an attempt to run but they were eventually apprehended and placed under police detainment.',
+			text: 'Since the trespasser refused to leave the area, we moved in to detain and arrest the suspect. They made an attempt to run but they were eventually apprehended anyway and placed under arrest.',
 		},
 		'No': {
-			text: 'Once we moved in to talk to the suspect, they did not make an attempt to run and we had them detained for the time being until we gained more information about the scene.',
+			text: 'Since the trespasser refused to leave the area, we moved in to detain and arrest the suspect. They did not make an attempt to run and we had them detained and placed under arrest.',
 		}
 	};
 	
 	let susranresult = susranSelected.options[susranSelected.selectedIndex].text;
 	buffer.push(susranInformation[susranresult].text);
-	
-	let testresultsSelected = document.getElementById('testresults');
-	let testresultsInformation = {
-		'Negative': {
-			text: 'Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back negative for any drug residue.',
-		},
-		'Minor amounts': {
-			text: 'Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back positive for minor amounts of drug residue.',
-		},
-		'Moderate amounts': {
-			text: 'Once we swabbed their hands for any possible drug residue and ran it through a test, the results came back positive for moderate amounts of drug residue.',
-		}
-	};
-	
-	let testresultsresult = testresultsSelected.options[testresultsSelected.selectedIndex].text;
-	buffer.push(testresultsInformation[testresultsresult].text);
-
-	let drugSelected = document.getElementById('drugtype');
-	let drugInformation = {
-		'Weed': {
-			text: 'The drug specifically in question was weed.',
-		},
-		'Cocaine': {
-			text: 'The drug specifically in question was cocaine.',
-		},
-		'Oxy': {
-			text: 'The drug specifically in question was oxy.',
-		},
-		'Acid': {
-			text: 'The drug specifically in question was acid.',
-		},
-		'Meth': {
-			text: 'The drug specifically in question was meth.',
-		}
-	};
-	
-	let drugresult = drugSelected.options[drugSelected.selectedIndex].text;
-	buffer.push(drugInformation[drugresult].text);
 	buffer.push('');
 	
 	let medicalSelected = document.getElementById('medicalattention');
@@ -108,11 +92,6 @@ function report() {
 	let medical = medicalSelected.options[medicalSelected.selectedIndex].text;
 	buffer.push(`[MEDICAL ATTENTION | ${medicalInformation[medical].label}]:`);
 	buffer.push(medicalInformation[medical].text);
-	buffer.push('');
-
-	let processed = document.getElementById('processedat').value;
-	buffer.push('[PROCESSED]:');
-	buffer.push(`All of the apprehended suspects were processed at ${processed}.`);
 	
 	let curDarkmode = document.getElementById('darkmode').checked;
 	if (curDarkmode) {
