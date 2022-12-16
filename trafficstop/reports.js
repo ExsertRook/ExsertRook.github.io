8303a646ea9a6e54c94ce3ddfc3bd541d8f6c5d1
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-12-16 20:48:36 +0200

    Update reports.js

--- trafficstop/reports.js
+++ trafficstop/reports.js
@@ -44,7 +44,7 @@ function report() {
 	buffer.push(stopreasonInformation[stopreason].text);
 	
 	let speed = document.getElementById('speed').value;
-	if (speed) buffer.push(`The exact speed we clocked the vehicle travelling at was ${speed}.`);
+	if (speed) buffer.push(`The exact speed we clocked the vehicle travelling at was ${speed} mph.`);
 	buffer.push('');
 	
 	let arrestreasonSelected = document.getElementById('arrestreason');
@@ -115,7 +115,7 @@ function report() {
 	return document.getElementById('reportBody').innerHTML = buffer.join("\n");
 }
 
-let inputs = document.querySelectorAll('input[type="text"], input[type="number"], textarea');
+let inputs = document.querySelectorAll('input[type="text"], input[type="number"], input[type="text2"], textarea');
 inputs.forEach(i => i.addEventListener('keyup', report, false));
 
 let checkboxes = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
@@ -165,85 +165,6 @@ function loadDarkmode() {
 
 let officers = null;
 let matched = [];
-
-const replaceNames = {
-	'Bucky Killbourne': 'Bucky Langston',
-	'Xander Langston': 'Xander Killbourne'
-};
-
-function loadOfficers() {
-	let cachedOfficers = localStorage.getItem("officers");
-	if (!officers) {
-		let xhr = new XMLHttpRequest();
-		try {
-			xhr.open("GET", "https://celestial.network/legacyrp/sasp", false);
-			xhr.send(null);
-
-			officers = JSON.parse(xhr.responseText).data;
-			officers = officers.map(officer => officer.callsign + ' ' +
-				(replaceNames[officer.full_name] ? replaceNames[officer.full_name] : officer.full_name));
-			localStorage.setItem('officers', xhr.responseText);
-		} catch (e) {
-			if (cachedOfficers) {
-				cachedOfficers = JSON.parse(cachedOfficers).data;
-				officers = cachedOfficers.map(officer => officer.callsign + ' ' + officer.full_name);
-				alert('Failed to load officers data from roster; using cached officers data...');
-			} else {
-				alert('Failed to load officers data from roster & no cache value stored!');
-			}
-		}
-	}
-}
-
-function searchOfficer(search) {
-	if (!search) {
-		document.getElementById('officerslist').innerHTML = '';
-		return;
-	}
-	search = search.toLowerCase();
-
-	if (!officers) loadOfficers();
-
-	let results = officers.filter(officer => officer.toLowerCase().includes(search));
-	let resultsCap = 5;
-	let count = 0;
-	let finalResults = [];
-	results.forEach(result => {
-		count++;
-		if (count > resultsCap) return;
-		result = result.trim();
-		finalResults.push("<button title='Add this officer to the list of officers involved' onClick='toggleOfficer(\"" + result + "\")'>" + result + "</button>");
-	});
-	document.getElementById('officerslist').innerHTML = finalResults.join("<br />");
-}
-
-function toggleOfficer(id) {
-	if (officersInvolved.has(id)) {
-		console.log("Removing " + id + "...");
-		officersInvolved.delete(id);
-	} else {
-		console.log("Adding " + id + "...");
-		officersInvolved.add(id);
-
-		document.getElementById('officersearch').value = "";
-	}
-	report();
-	updateOfficers();
-}
-
-function updateOfficers() {
-	let output = "";
-	for (let id of officersInvolved.values()) {
-		output += `<div class="chip">\n`;
-		output += `<img src="images/hat2.png" width="96" height="96">\n`;
-		output += `${id}\n`;
-		output += `<span class="closebtn" title="Remove this officer from the list of officers involved" style="cursor: default;" onclick='toggleOfficer(\"${id}\")'><i class="fa fa-times-circle-o" aria-hidden="true"></i>
-</span>\n`;
-		output += `</div>`
-	}
-
-	document.getElementById('officersAdded').innerHTML = "<br />" + output;
-}
 
 function showCopiedPopup() {
 	let popup = document.getElementById("myPopup");
