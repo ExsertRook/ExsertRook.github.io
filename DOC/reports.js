79c790b579f8f90629768b05542fca45f5bd00d0
ExsertRook <96119259+ExsertRook@users.noreply.github.com>
2022-12-17 23:49:11 +0200

    Add files via upload

---
+++ DOC/reports.js
@@ -0,0 +1,127 @@
+'use strict';
+
+let buffer = [];
+let officersInvolved = new Set();
+let darkmodeState;
+let alreadySpecifiedRobbery = false;
+let ROBBERY_STATE = 'JEWLERY';
+
+function report() {
+	let callsign = document.getElementById('yourself').value.trim();
+	if (callsign) localStorage.setItem('callsign', callsign);
+	if (!callsign) callsign = '[missing]';
+	const ind = "        ";
+	let date = new Date().toLocaleDateString('en-US');
+
+	buffer = [];
+
+	let InmateName = document.getElementById('inmatename').value;
+	let ContrabandFound = document.getElementById('contraband').value;
+	let ContrabandStatus = document.getElementById('status').value;
+	let ConfiscatedPockets = document.getElementById('confiscatedpockets').value;
+
+	if (InmateName) buffer.push(`**Inmate Name:** ${InmateName}`);
+	if (ContrabandFound) buffer.push(`**Contraband Found:** ${ContrabandFound}`);
+	if (ContrabandStatus) buffer.push(`**Status:** ${ContrabandStatus}`);
+	buffer.push('');
+	if (callsign) buffer.push(`**Signed**`);
+	if (callsign) buffer.push(`*${callsign}*`);
+	if (ConfiscatedPockets) buffer.push(`${ConfiscatedPockets}`);
+	
+	let curDarkmode = document.getElementById('darkmode').checked;
+	if (curDarkmode) {
+		if (darkmodeState === 'false') updateDarkmode();
+	} else if (!curDarkmode) {
+		if (darkmodeState === 'true') updateDarkmode();
+	}
+
+	return document.getElementById('reportBody', 'reportBody2').innerHTML = buffer.join("\n");
+}
+
+let inputs = document.querySelectorAll('input[type="text"], input[type="number"], input[type="text2"], textarea');
+inputs.forEach(i => i.addEventListener('keyup', report, false));
+
+let checkboxes = document.querySelectorAll('input[type="checkbox"], input[type="radio"]');
+checkboxes.forEach(i => i.addEventListener('click', report, false));
+
+let selectOptions = document.querySelectorAll('select');
+selectOptions.forEach(i => i.addEventListener('click', report, false));
+
+function loadName() {
+	let callsign = '';
+	if (localStorage.getItem('callsign')) callsign = localStorage.getItem('callsign');
+	document.getElementById('yourself').value = callsign;
+}
+
+// Listen for a click on the button
+function updateDarkmode() {
+	// Then toggle (add/remove) the .dark-theme class to the body
+	let darkmode = document.getElementById('darkmode').checked;
+	if (darkmode) {
+		localStorage.setItem("darkmode", true);
+		darkmodeState = 'true';
+	} else if (!darkmode) {
+		localStorage.setItem("darkmode", false);
+		darkmodeState = 'false';
+	}
+	document.body.classList.toggle('dark-theme');
+}
+
+function loadDarkmode() {
+	let darkmodeSetting = localStorage.getItem("darkmode");
+	if (!darkmodeSetting || darkmodeSetting === 'undefined' || darkmodeSetting === 'false') {
+		localStorage.setItem("darkmode", false);
+		darkmodeState = 'false';
+	}
+	if (darkmodeSetting == 'true') {
+		document.getElementById('darkmode').checked = true;
+		document.body.classList.toggle('dark-theme');
+		darkmodeState = 'true';
+	}
+	loadName();
+	if (ROBBERY_STATE === 'JEWLERY') {
+		document.getElementById('whatFleeca').style.display = 'none';
+		document.getElementById('whatStore').style.display = 'none';
+	}
+	//loadOfficers();
+}
+
+let officers = null;
+let matched = [];
+
+function showCopiedPopup() {
+	let popup = document.getElementById("myPopup");
+	popup.classList.toggle("show");
+	setTimeout(function() {
+		popup.classList.toggle("show");
+	}, 3500);
+}
+
+document.getElementById('copyReport').addEventListener('click', copy, false);
+function clearSelection() {
+	if (window.getSelection) {
+		window.getSelection().removeAllRanges();
+	} else if (document.selection) {
+		document.selection.empty();
+	}
+}
+function copy() {
+	document.getElementById('reportBody').select();
+	try {
+		document.execCommand('copy');
+		showCopiedPopup();
+		clearSelection();
+	} catch(e) {
+		console.log("Copy error: " + e);
+	}
+}
+function copy2() {
+	document.getElementById('reportBody2').select();
+	try {
+		document.execCommand('copy');
+		showCopiedPopup();
+		clearSelection();
+	} catch(e) {
+		console.log("Copy error: " + e);
+	}
+}
